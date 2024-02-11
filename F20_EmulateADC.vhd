9adad2eaf5b7b3ed621a7cd9ccb3df79f407140e
etabli-i5 <>
2024-02-11 21:09:38 +0100

    big update

--- F20_EmulateADC.vhd
+++ F20_EmulateADC.vhd
@@ -31,7 +31,9 @@ port(
   -- Test output only
   tst_CLEAR_REG     : out std_logic ;
   tst_CLEAR_MOY     : out std_logic ;
-  tst_SCK_count    : out integer range 0 to 32
+  tst_SCK_count    : out integer range 0 to 32;
+  tst_loadSR		: out std_logic;
+  tst_READ_end		: out std_logic 
 );
 
 end F20_EmulateADC;
@@ -46,22 +48,27 @@ signal  SRDATA      : std_logic_vector(23 downto 0);
 signal  SSCK        : std_logic ;
 
 signal  QA          : std_logic ;
-signal  QB         : std_logic ;
+signal  QB          : std_logic ;
 signal  CLEAR_REG   : std_logic ;
 signal  CLEAR_MOY   : std_logic ;
 
 signal  CLS_count   : std_logic ;
-signal  CLS        : std_logic ;
+signal  CLS         : std_logic ;
 
 signal SCK_count    : integer range 0 to 32 ;
+
+signal LOAD_SR      : std_logic ;
+signal READ_end     : std_logic ;
+
 
 begin
 
 --- Copy of signals for tests purpose
 tst_CLEAR_REG <= CLEAR_REG  ; -- TEst output
 tst_CLEAR_MOY <= CLEAR_MOY  ; -- TEst output
-tst_SCK_count  <= SCK_count ; -- TEst output
-
+tst_SCK_count <= SCK_count  ; -- TEst output
+tst_loadSR	  <= LOAD_SR    ;
+tst_READ_end  <= READ_end   ;
 
 ------------------------------------------------------------------
 -- Function to emulate busy of DAC for simulation purpose
@@ -78,7 +85,7 @@ begin
       if    rising_edge(MCLK) then
             if   BUSY_on='1' then
               Busy_count <= Busy_count + 1 ; --
-                if    Busy_count= 35  then -- 35x10ns = 350ns pulse width
+                if    Busy_count= 35  then -- 35x10ns =350ns pulse width 
                       Busy_end <= '1'  ; --
                 else
                       Busy_end <= '0'  ; --
@@ -99,7 +106,7 @@ end process Emulate_Busy ;
 -- génére des valeurs
 -- en série à la place de SDOR et SDOL (valeurs série des ADC).
 ------------------------------------------------------------------
-Emulate_SDO : process(CNV,DATADC,SCK,SRDATA,SSCK,MCLK,CLEAR_REG,QA,QB,CLEAR_MOY,SCK_count)
+Emulate_SDO : process(CNV,DATADC,SCK,SRDATA,SSCK,MCLK,CLEAR_REG,QA,QB,CLEAR_MOY,SCK_count,READ_end,LOAD_SR)
 begin
       -- Reset if no SCK betweeb 2 CNV conversion pulse.
       if    SCK='1'            then
@@ -111,20 +118,34 @@ begin
       end if;
       CLEAR_MOY <=  QB and CNV  ;
       --
-      -- Check number of SCK shift
-      if      CNV='1' and SCK_count=23 then
+
+      --PARTIE A REVOIR !! en dessous.
+
+     -- Check number of SCK shift
+       if      (CNV='1' and SCK_count=23) or CLEAR_MOY='1'  then -- ON le 08/02/24 : on reset aussi le compteur SCK_count si clear_moy est actif
                SCK_count <= 0   ;
                CLEAR_REG <= '0'  ;
       elsif    rising_edge(SCK) then
+            --
+            if    SCK_count>=22  then
+                  READ_end <= '1' ;
+            else
+                  READ_end <= '0' ;
+            end if;
+            --
             if    SCK_count=23  then
                   CLEAR_REG <= '1'  ;
             else
                   SCK_count <= SCK_count +1 ;
             end if;
+
       end if;
 
+      LOAD_SR <= CNV and READ_end ;--
+
       -- Shift register update  
-      if    CLEAR_MOY='1' or  CLEAR_REG='1' then
+      if    CLEAR_MOY='1' or  LOAD_SR='1' then
+      --if    CNV='1'  then -- test
             SRDATA <= DATADC;
       elsif rising_edge(SCK)   then
             SRDATA <= SRDATA(22 downto 0) & '0'  ; -- décalage d'un bit à chaque front montant de SCK
